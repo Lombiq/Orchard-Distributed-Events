943e1a2b3e478d056d4ea98541112af2c7a1f9ce
Zoltán Lehóczky <lehoczky_zoltan@pyrocenter.hu>
2014-08-04 11:22:33 +0200

    Adding note about possible SO

--- Filters/PollingEventRaisingApiFilter.cs
+++ Filters/PollingEventRaisingApiFilter.cs
@@ -9,6 +9,8 @@ using Orchard.WebApi.Filters;
 
 namespace Lombiq.Hosting.DistributedEvents.Filters
 {
+    // This filter can cause a stack overflow without actually doing anything because of a suspected .NET
+    // framework bug, see: https://orchard.codeplex.com/discussions/558025
     [OrchardFeature("Lombiq.Hosting.DistributedEvents.ForegroundPollingEventRaising")]
     public class PollingEventRaisingApiFilter : ActionFilterAttribute, IApiFilterProvider
     {
